6d39ca1697f94a9cbc9585ec1ba02fb345455a92
ibtihal amarate <ibtihalamarate02@gmail.com>
2024-12-21 15:02:39 +0100

    Modification de jenkinsfiles

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,25 +34,17 @@ pipeline {
             }
         }
 
-        stage('Push to Docker Hub') {
-            steps {
-                script {
-                    echo 'Pushing Docker image to Docker Hub'
-                    withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                        sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
-                        sh 'docker push $DOCKER_IMAGE'
-                    }
-                }
+    stage('Push to Docker Hub') {
+    steps {
+        script {
+            echo 'Pushing Docker image to Docker Hub'
+            withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                sh 'docker push $DOCKER_IMAGE'
             }
-            /*
-        stage('Deploy to Remote Server') {
-            steps {
-                script {
-                    echo 'Deploying to remote server'
-                    // Commande pour déployer votre projet, par exemple via SCP
-                    sh 'scp -r ./target/* user@remote-server:/path/to/deployment'
-                }
-            }
-        }*/
+        }
+    }
+}
+
     }
 }
